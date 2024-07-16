c980f64f3c074c17c786937d51baa62994a41b85
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-07-17 01:22:54 +0530

    Added Environment Variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-bold ">Hello World!</h1>
