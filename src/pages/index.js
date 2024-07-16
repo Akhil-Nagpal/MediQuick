17fb6383762167d82d54a0844feb1fb04236e953
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-07-17 03:45:30 +0530

    Added Pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Shop from "./Shop";
+import SingleProduct from "./SingleProduct";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+export { Home, About, Contact, Shop, SingleProduct, Login, SignUp };
