975a5afecce838d2f2807d636b852a0b57761571
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-07-19 21:14:50 +0530

    Added Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#59A52C",
+        secondary: "#6FCA3A",
+        accent: "#E6E6E6",
+        background: "#FFFFFF",
+        text: "#1E2022",
+      },
+    },
   },
   plugins: [],
 };
+// #59A52C - PRIMARY
+// #6FCA3A - SECONDARY
+// #E6E6E6 - ACCENT
+// #FFFFFF - BACKGROUND
+// #1E2022 - TEXT
