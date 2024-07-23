a70371c39f1be49989e8e0ed3d9909eaf9b3a717
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-07-23 21:17:07 +0530

    Added Hero Banner

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HeroBanner() {
+  return (
+    <>
+      <div className="flex bg-red-500">
+        <div className="bg-green-500"></div>
+        <div className="bg-green-500"></div>
+      </div>
+    </>
+  );
+}
+
+export default HeroBanner;
